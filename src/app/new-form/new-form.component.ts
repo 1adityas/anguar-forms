172a600d1c470370fff854b026d712fb74e8af56
aditya pratap singh <1adityapsingh@gmail.com>
2022-05-28 22:56:57 +0530

    forms angular

---
+++ src/app/new-form/new-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormArray,Validator, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-form',
+  templateUrl: './new-form.component.html',
+  styleUrls: ['./new-form.component.css']
+})
+export class NewFormComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+  }
+
+  get alternateEmails(){
+    console.log(this.registrationForm.get('alternateEmail') )
+    return this.registrationForm.get('alternateEmail')  as FormArray
+  }
+  registrationForm=this.fb.group({
+    firstName:['aditya',[Validators.required,Validators.minLength(3)]],
+    lastName:[],
+    email:[],
+    password:[],
+    confirmPassword:[],
+    alternateEmail:this.fb.array([])
+
+  },{Validators})//we will add validators
+  addAlternateEmail(){
+    this.alternateEmails.push(this.fb.control('')) 
+  }
+
+  //  registrationForm=new FormGroup({
+  //    firstName:new FormControl(''), 
+  //    lastName:new FormControl(''), 
+  //    email:new FormControl(''), 
+  //    password:new FormControl(''), 
+  //    confirmPassword:new FormControl(''), 
+  //  })
+}
---
+++ src/app/shared/password.valid.ts
@@ -0,0 +1,5 @@
+import { AbstractControl } from "@angular/forms";
+
+function passwordValidator(control:AbstractControl):{[key:string]:any}| null{\
+
+}
